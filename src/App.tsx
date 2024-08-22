7fc05a58aacabe4d69a739a83bb77de5244d40e9
pql-lb <lbaker1594@hotmail.com>
2024-08-22 16:35:21 +0100

    addition of error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import { Loader } from "./components/molecules/Loader";
+import ErrorBoundary from "./components/molecules/Error";
 const Products = lazy(() => import("./components/pages/products"));
 
 function App() {
     return (
         <Router>
-            <Suspense fallback={<Loader />}>
-                <Routes>
-                    <Route path="/" element={<Products />} />
-                </Routes>
-            </Suspense>
+            <ErrorBoundary>
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        <Route path="/" element={<Products />} />
+                    </Routes>
+                </Suspense>
+            </ErrorBoundary>
         </Router>
     );
 }
