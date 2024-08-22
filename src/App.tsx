55caad58906155534b90f882a9b05dc57f7a8aac
pql-lb <lbaker1594@hotmail.com>
2024-08-22 16:36:55 +0100

    addition of not found page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import { Loader } from "./components/molecules/Loader";
 import ErrorBoundary from "./components/molecules/Error";
+const NotFound = lazy(() => import("./components/pages/NotFound"));
 const Products = lazy(() => import("./components/pages/products"));
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Suspense fallback={<Loader />}>
                     <Routes>
                         <Route path="/" element={<Products />} />
+                        <Route path="*" element={<NotFound />} />
                     </Routes>
                 </Suspense>
             </ErrorBoundary>
