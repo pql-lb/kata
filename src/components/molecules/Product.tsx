fa2c5d800c1df96701a856fb5138ff0ea543406d
pql-lb <lbaker1594@hotmail.com>
2024-08-24 19:22:12 +0100

    product item display pricing

--- src/components/molecules/Product.tsx
+++ src/components/molecules/Product.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useContext } from "react";
 import { Structure } from "../../types/main";
 import { DispatchContext, actionTypes } from "../../context/store";
 import { Button } from "../atoms/Button";
+import { formatPrice } from "../../utils/main";
 
 type Props = {
     product: Structure;
@@ -9,17 +10,27 @@ type Props = {
 };
 
 export const Product = React.memo(({ product, index }: Props) => {
-    const { id } = product;
+    const { id, unitPrice, specialPrice } = product;
     const dispatch = useContext(DispatchContext);
     const handleClick = useCallback(() => {
         if (dispatch) {
             dispatch({ type: actionTypes.UPDATE_CART, payload: product });
         }
     }, [dispatch]);
+    console.log(specialPrice);
     return (
         <>
             <div className="product">
                 <h2 className="product__title">{id}</h2>
+                <p className="product__price">{formatPrice(unitPrice)}</p>
+                {specialPrice ? (
+                    <p className="product__discount">
+                        Buy {specialPrice.quantity} for{" "}
+                        {formatPrice(specialPrice.price)}
+                    </p>
+                ) : (
+                    <></>
+                )}
                 <Button string="Add to Cart" handleClick={handleClick} />
             </div>
         </>
